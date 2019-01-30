418908a95482731095a6085eaaf84a9bc2ff7b6c
wutianning <wutianning@DESKTOP-OPSBB5N>
2019-01-30 17:37:44 +0800

    武天宁

--- src/main/java/com/zkr/api/pojo/User.java
+++ src/main/java/com/zkr/api/pojo/User.java
@@ -1,17 +1,22 @@
 package com.zkr.api.pojo;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class User {
 	
+	
+	private BigDecimal userid;
+	
 	private String username;
 	
 	private String userpwd;
 	
-	private String verifycode;   //验证码
+	//private String verifycode;   //验证码
 	
-	private String usertel;      //电话
+	//private String usertel;      //电话
 	
 
 }
